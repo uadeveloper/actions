f6b84c8e68a4dbacf9556146012dd6553160cb02
Alexey Sh <alexeysh@ronis.mail>
2022-11-07 21:25:03 +0200

    Commit

---
+++ Tests/Catalog/CatalogTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class CatalogTest extends TestCase
+{
+    public function test(): void
+    {
+        $this->assertTrue(true);
+        $this->assertEquals(0, 0);
+    }
+
+    public function testCustomName(): void
+    {
+        $this->assertTrue(true);
+        $this->assertEquals(0, 0);
+    }
+
+}
---
+++ test_bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "test_bootstrap.php...\n";
